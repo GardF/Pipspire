8da423a2104c72ad7de8cc993533172a18b3eeed
GardF <gard.fjer99@gmail.com>
2025-11-27 23:07:23 +0100

    Continued with tutorial

--- main.lua
+++ main.lua
@@ -3,20 +3,30 @@ if arg[2] == "debug" then
 end
 
 
---[[
 function love.load()
-  x = 100
+listOfRectangles = {} 
 end
 
+function createRect()
+    rect = {}
+    rect.x = 100
+    rect.y = 100
+    rect.width = 100
+    rect.height = 70
+    rect.speed = 100
+    table.insert(listOfRectangles,rect)
+end
+   
+
 function love.update(dt)
-    if l
+    rect.x = rect.x + rect.speed * dt
 end
 
 function love.draw()
-    love.graphics.rectangle("line", x, 50, 200, 150)
+    love.graphics.rectangle("fill",rect.x, rect.y, rect.width, rect.height)
 end
 
---]]
+
 
 
 

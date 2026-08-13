cb61829e501ddc05218688b3a04aa1d28f5d4557
GardF <gard.fjer99@gmail.com>
2026-08-13 14:07:49 +0200

    Added sprite sheet for dominoes and added quads to draw the dominoes from the sprite sheet.

--- main.lua
+++ main.lua
@@ -1,24 +1,37 @@
 
+--! file: main.lua
 
 function love.load()
     
     Domino = {}
-    Domino.left = 0
-    Domino.right = 0
+    Domino.left_piece_value = 0 
+    Domino.right_piece_value = 0
+
+
+
+    local sheet = love.graphics.newImage("domino_sheet.png")
+    local domW, domH = 50, 100
+    local quads = {}
+
+    for i = 0, 6 do
+    quads[i+1] = love.graphics.newQuad(
+        i * domW, 0,      -- x, y position in the sheet
+        domW, domH,        -- width, height of this cell
+        sheet:getDimensions()
+    )
+end
+
 
     Random_pip_left = love.math.random(0,6)
     Random_pip_right = love.math.random(0,6)
-    
 
-    Domino.left = Random_pip_left
-    Domino.right = Random_pip_right
+
+
+
+    
 
 end
 
 function love.draw()
-    love.graphics.print("Train", 400, 100)
-
-    love.graphics.print(Domino.left, 200, 300)
-    love.graphics.print(Domino.right, 400, 300)
-
+    love.graphics.draw(sheet, quads[3], x, y)
 end
